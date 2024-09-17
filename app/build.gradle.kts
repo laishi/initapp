baad1fafd71e9ba35dc923f5b9d4f2d7e4bc9afc
laishi <laishi886@hotmail.com>
2024-09-17 17:49:19 +0800

    run

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,21 +27,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
 }
 
-// dependencies {
-//     implementation("androidx.core:core-ktx:1.9.0")
-//     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-//     implementation("androidx.activity:activity-compose:1.8.0")
-//     implementation("androidx.compose.ui:ui")
-//     implementation("androidx.compose.ui:ui-graphics")
-//     implementation("androidx.compose.ui:ui-tooling-preview")
-//     implementation("androidx.appcompat:appcompat:1.6.1")
-//     implementation("androidx.compose.material3:material3:1.1.2")
-
-//     implementation("com.google.accompanist:accompanist-flowlayout:0.26.2-beta")
-
-//     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-// }
-
 android {
      buildTypes {
         getByName("release") {
